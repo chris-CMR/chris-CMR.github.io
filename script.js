88ce8374f07856155abaf0898ad6b318953beb8d
Christopher Macedonio Rios <macedoniochristopher140@gmail.com>
2025-07-30 21:53:27 -0700

    made the button into a counter

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var counter = 0;
 
 function changeText() {
     counter++;
-    document.getElementById("dynamic-text").innerText = `You clicked the button ${counter} times!`;
+    document.getElementById("dynamic-text").innerText = `you clicked the button ${counter} times!`;
 }
